6699ed43b13c11f988f3b5edbd25d262b9ab5170
Bryant Wang and taylor loftis-kim <bryant+taylor@>
2016-11-29 13:39:12 -0800

    initial commit

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,15 @@
+using Allergy.Objects;
+using System.Collections.Generic;
+using System;
+using Nancy;
+
+namespace Allergy
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+
+    }
+  }
+}
